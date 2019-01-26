9818ebea2e64ac077ffa43adf1d04c49dc733a1a
YutoMasamura <yutomasamura@YutoMasamuranoMacBook-puro.local>
2019-01-26 12:13:44 +0900

    コメント追加

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,8 +1,9 @@
 class BonusDrink
   def self.total_count_for(amount)
-  	# 3本未満の場合はシンプルにamount の値が答えとなる
+    # 3本未満の場合はamountの値が答え
     return amount if amount < 3
-    # 3本以上の場合はプレゼントの本数を加えた値が答えとなる
+
+    # 3本以上の場合はamountにおまけの本数を加えた値が答え
     return amount + (amount - 1) / 2 if amount >= 3
   end
 end
